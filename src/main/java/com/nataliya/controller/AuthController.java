827171a2caf176e288ff8c3cc8b8f72808125d73
Nataliya <n.basanina@gmail.com>
2026-01-13 00:00:47 +0300

    feat: add auto authentication after registration in AuthController

--- src/main/java/com/nataliya/controller/AuthController.java
+++ src/main/java/com/nataliya/controller/AuthController.java
@@ -3,12 +3,19 @@ package com.nataliya.controller;
 import com.nataliya.dto.UserRegistrationRequestDto;
 import com.nataliya.dto.UsernameResponseDto;
 import com.nataliya.service.RegistrationService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,23 +24,35 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final RegistrationService registrationService;
+    private final AuthenticationManager authenticationManager;
+    private final SecurityContextRepository securityContextRepository;
 
     @PostMapping("/sign-up")
     public ResponseEntity<UsernameResponseDto> register(
-            @Valid @RequestBody UserRegistrationRequestDto userRequestDto
+            @Valid @RequestBody UserRegistrationRequestDto userRequestDto,
+            HttpServletRequest request,
+            HttpServletResponse response
     ) {
 
         UsernameResponseDto userResponseDto = registrationService.registerUser(userRequestDto);
+
+        UsernamePasswordAuthenticationToken authRequest = UsernamePasswordAuthenticationToken
+                .unauthenticated(
+                        userRequestDto.username(),
+                        userRequestDto.password()
+                );
+
+        Authentication auth = authenticationManager.authenticate(authRequest);
+
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        securityContext.setAuthentication(auth);
+
+        securityContextRepository.saveContext(securityContext, request, response);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(userResponseDto);
-    }
-
-    @GetMapping("/me")
-    public Authentication me(Authentication authentication) {
-        return authentication;
     }
 
 }
