329c12e7a1944b29ccfac9387f1b687026346166
Nataliya <n.basanina@gmail.com>
2026-01-18 01:45:04 +0300

    feat: create UserController with getUser method

---
+++ src/main/java/com/nataliya/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.nataliya.controller;
+
+import com.nataliya.dto.UsernameResponseDto;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @GetMapping("/me")
+    public UsernameResponseDto getUser(@AuthenticationPrincipal UserDetails userDetails) {
+        return new UsernameResponseDto(userDetails.getUsername());
+    }
+}
